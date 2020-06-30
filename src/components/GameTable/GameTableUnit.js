0d41aef352110aa6aee94adf970cb38ebe8a267b
mefa <mfbyrktr@gmail.com>
2020-07-01 00:17:14 +0300

    Alpha 0.0.2

--- src/components/GameTable/GameTableUnit.js
+++ src/components/GameTable/GameTableUnit.js
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GameTable.scss";
 
 const GameTableUnit = () => {
-    const data_tableWords = "aedhaümksbecfite"
+    const data_tableChars = "aedhaümksbecfite"
     const data_validAnswers=["sit","bek","cet","asi","bet","met","dem","kem","ece","tek","eti","ket","tem","fit","bas","sif","bit","cem","ekme","beis","emek","deme","süet","düet","sabi","site","kete","keme","asit","beti","abis","emet","etek","sübek","tebaa","emcek","demek","metis","basit","sümek","demet","bitek","sabit","tekme","abece","sitem","ecet","sabite","isabet"]
-	//const tableData = ["A", "B", "C","D","E","F","G","Ğ","H","I","İ","J","K","L","M","N","O","Ö","P","R","S","Ş","T","U","Ü","V","Y","Z"]
-    
+    const [correctAnswers, setCorrectAnswers] = useState([])
+    const [currentAnswer, setCurrentAnswer] = useState("")
+
+    const handleChange = (e) => {
+        setCurrentAnswer(e.target.value)
+        /* console.log(currentAnswer) */
+    }
+    const handleAnswer = () => {
+        console.log(currentAnswer)
+        if(data_validAnswers.includes(currentAnswer)){
+            console.log("Congrats")
+            setCorrectAnswers([...correctAnswers,currentAnswer])
+            setCurrentAnswer("")
+        }
+        /* setCorrectAnswers(e.target.value) */
+    }
     return (
 		<div className="game_container">
 			<div className="game_container_inner">
-				<div className="game_chars">A</div>
-				<div className="game_chars">B</div>
-				<div className="game_chars">C</div>
-				<div className="game_chars">D</div>
-				<div className="game_chars">E</div>
-				<div className="game_chars">F</div>
-				<div className="game_chars">G</div>
-				<div className="game_chars">H</div>
-				<div className="game_chars">K</div>
-				<div className="game_chars">O</div>
-				<div className="game_chars">H</div>
-				<div className="game_chars">K</div>
-				<div className="game_chars">K</div>
-				<div className="game_chars">O</div>
-				<div className="game_chars">H</div>
-				<div className="game_chars">K</div>
+                {
+                    data_tableChars.split("").map((char,index)=>{
+                        return<div key={index} className="game_chars">{char.toUpperCase()}</div>
+                    })
+                }
 			</div>
 
-			<div className="game_answer">
-				<input type="text" />
-                <button type="submit">Enter</button>
+            <div className="game_correct_answers">
+                Answers
+            {correctAnswers.length?correctAnswers.map((answer,index)=>(<div key={index}>{answer}</div>)):null}
+            </div>
+
+			<div className="game_input_answer">
+                <input 
+                    type="text" 
+                    value={currentAnswer} 
+                    onChange={handleChange}
+                    onKeyPress = {(event)=>(event.charCode === 13)?handleAnswer():null}
+                    />
+                    
+                <button type="submit" onClick={handleAnswer}>Enter</button>
 			</div>
 		</div>
 	);
