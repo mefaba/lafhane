9d7757aad8a7538dcd39f3fbb46278c3ba88f076
mefa <mfbyrktr@gmail.com>
2020-08-27 00:13:45 +0300

    Loading ekranı

--- src/components/GameIntro/GameIntroUnit.js
+++ src/components/GameIntro/GameIntroUnit.js
@@ -1,19 +1,26 @@
 import React from "react";
-import "./GameIntro.scss";
+
 import axios from "axios";
 import { useContext } from "react";
 import { GameContext } from "../../context/GameContext";
 import { useState } from "react";
 
+import "./GameIntro.scss";
+import LottieLoading from "./LottieLoading.js"
+
 function GameIntroUnit({ setIsStarted }) {
 	const { username, setUsername } = useContext(GameContext);
 	const [message, setMessage] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
 
 	const sendUsernameToAPI = () => {
 		axios.post(`${process.env.REACT_APP_ACTIVESERVER}/api/users`, { username: username }).then(({ data }) => {
-			//then if response is positive setIsStarted => True / Else if negative ask new user name
+			//then if response is true setIsStarted => True / Else if negative ask new user name
 			if (data) {
 				setIsStarted(false);
+				setTimeout(() => { //Giriş başarısız olsa dahi 3 saniye sonra loading ekranı gitsin
+					setIsLoading(false)
+				}, 3000);
 			} else {
 				setIsStarted(true);
 			}
@@ -24,6 +31,7 @@ function GameIntroUnit({ setIsStarted }) {
 		if (username.length > 3 && username.length < 11) {
 			//check if username exists in database.
 			sendUsernameToAPI();
+			setIsLoading(true)
 			setMessage(
 				"Hoşgeldiniz yada Tekrar Deneyin. Bu isimde bir kullanıcı zaten oyunda olabilir. Farklı bir isim deneyin."
 			);
@@ -32,9 +40,13 @@ function GameIntroUnit({ setIsStarted }) {
 		/* console.log(username) */
 	};
 
+
 	return (
 		<div className="gameintro_container">
 			<h1>GAME INTRO</h1>
+
+			{isLoading?<LottieLoading/>:
+			(<>
 			<p>Min length: 4, Max Length: 10</p>
 			<label htmlFor="username">Username: </label>
 			<input
@@ -47,6 +59,9 @@ function GameIntroUnit({ setIsStarted }) {
 			<br></br>
 			<button onClick={handleStart}>Start Game</button>
 			<p>{message}</p>
+			</>)}
+
+			
 		</div>
 	);
 }
---
+++ src/components/GameIntro/LottieLoading.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Lottie from 'react-lottie-player'
+
+import lottieJson from "../../assets/LottieFiles/loading.json"
+
+export default class LottieLoading extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {  };
+	}
+	render() {
+
+
+		return (
+			<div>
+                <Lottie
+                    loop
+                    animationData={lottieJson}
+                    play
+                    style={{ width: 200, height: 200, margin: "auto" }}
+                />
+			</div>
+		);
+	}
+}
